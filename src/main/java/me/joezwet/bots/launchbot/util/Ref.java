29a1adf350c61376bd558d8327453584199f8954
JoeZwet <zwet.joe@projectpluto.us>
2017-05-12 06:46:16 +1200

    Update Ref.java

--- src/main/java/me/joezwet/bots/launchbot/util/Ref.java
+++ src/main/java/me/joezwet/bots/launchbot/util/Ref.java
@@ -2,8 +2,8 @@ package me.joezwet.bots.launchbot.util;
 
 public class Ref {
 	
-	public static final String BOT_TOKEN = "MzExNzc4NjY4NTAwMzUzMDI0.C_TxSQ.LUyf724IROSutXAQexmejclN5Jk";
-	public static final String BOT_INVITE_URL = "https://discordapp.com/oauth2/authorize?client_id=311778668500353024&scope=bot&permissions=268454912";
-	public static final String API_BASE_URL = "";
+	public static final String BOT_TOKEN = "TOKEN";
+	public static final String BOT_INVITE_URL = "https://discordapp.com/oauth2/authorize?client_id=CLIENT_ID&scope=bot&permissions=268454912";
+	public static final String API_BASE_URL = "https://launchlibrary.net/1.2";
 
 }
