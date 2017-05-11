29ff09e63b6193747cefb85020372e0e05f392d7
JoeZwet <zwet.joe@projectpluto.us>
2017-05-12 06:50:12 +1200

    Update LaunchBot.java

--- src/main/java/me/joezwet/bots/launchbot/LaunchBot.java
+++ src/main/java/me/joezwet/bots/launchbot/LaunchBot.java
@@ -13,8 +13,13 @@ public class LaunchBot {
 	
 	public static void main(String[] args) {
 		try {
+			if(Ref.isBot) {
 			JDA api = new JDABuilder(AccountType.BOT).setToken(Ref.BOT_TOKEN).addEventListener(new NextLaunches()).buildBlocking();
 			api.getPresence().setGame(Game.of("To the SKY!"));
+			} else {
+			JDA api = new JDABuilder(AccountType.CLIENT).setToken(Ref.BOT_TOKEN).addEventListener(new NextLaunches()).buildBlocking();
+			api.getPresence().setGame(Game.of("To the SKY!"));
+			}
 		} catch (LoginException | IllegalArgumentException | InterruptedException | RateLimitedException e) {
 			e.printStackTrace();
 		}
