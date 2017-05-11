53121dbf96fab885224885e0f892f85526a173f9
JoeZwet <zwet.joe@projectpluto.us>
2017-05-12 06:49:20 +1200

    Update Ref.java

--- src/main/java/me/joezwet/bots/launchbot/util/Ref.java
+++ src/main/java/me/joezwet/bots/launchbot/util/Ref.java
@@ -3,6 +3,8 @@ package me.joezwet.bots.launchbot.util;
 public class Ref {
 	
 	public static final String BOT_TOKEN = "TOKEN";
+	//set to false if you are running it off a user account not a bot account
+	public static final boolean isBOT = true;
 	public static final String BOT_INVITE_URL = "https://discordapp.com/oauth2/authorize?client_id=CLIENT_ID&scope=bot&permissions=268454912";
 	public static final String API_BASE_URL = "https://launchlibrary.net/1.2";
 
